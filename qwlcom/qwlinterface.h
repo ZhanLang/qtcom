f321ded41f39eb7e35340fd5ee5ff7caa3321a24
maguojun <maguojun123@126.com>
2021-02-22 19:35:20 +0800

    1

--- qwlcom/qwlinterface.h
+++ qwlcom/qwlinterface.h
@@ -22,3 +22,14 @@ MS_DEFINE_IID(IClassFactory,"{00000001-0000-0000-C000-000000000046}");
 
 #define IWLComBase  IUnknown
 #define INULL       ((IUnknown*)0)
+
+struct IWLClassFactory: public IClassFactory
+{
+public:
+    STDMETHOD(CreateInstance)(IWLComBase *prot, IWLComBase *punkOuter, const IID& riid, void **ppvObject) = 0;
+    STDMETHOD_(CLSID, GetAt)(LONG nIndex) = 0;
+    STDMETHOD_(LONG, GetCount)() = 0;
+    STDMETHOD_(const char*, ProgIDFromCLSID)(REFCLSID clsid) = 0;
+
+};
+MS_DEFINE_IID(IMSClassFactory,"{6966E385-DBFA-4131-A29E-D0E9464F3F53}");
